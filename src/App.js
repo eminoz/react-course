84bb2aec589c2a720c74f93de9e47ed6de368ee9
eminoz <emin@Emins-MacBook-Pro.local>
2021-09-07 19:22:26 +0300

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,14 +6,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(()=>{
+    const storedUserLoggedInInformation=localStorage.getItem("isLoagedIn")
 
+  if(storedUserLoggedInInformation==="1"){
+    setIsLoggedIn(true);
+  }},[])
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoagedIn","1")
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoagedIn")
     setIsLoggedIn(false);
   };
 
